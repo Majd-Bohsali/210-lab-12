ea0c35b02e32e99970ea5b4e6ac0959dae00c849
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-09-19 18:19:46 -0700

    opens file in read

--- 210-lab-12.cpp
+++ 210-lab-12.cpp
@@ -7,8 +7,21 @@
 #include <array>
 #include <fstream>
 #include <iostream> 
+#include <string> 
 using namespace std;
 
 int main() { 
+    ifstream inputFile("studentTestScores.txt"); 
+    
+    if(!inputFile.is_open()) { 
+        cerr << "Error in opening file"; 
+    }
+
+    string line; 
+    while(getline(inputFile, line)) { 
+        cout << line << endl; 
+    }
+
+    inputFile.close();
     return 0;
 }
