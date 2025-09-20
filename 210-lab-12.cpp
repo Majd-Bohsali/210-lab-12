b06f9e67bad5ca34b5f51d5eedcb46fce7a1e44d
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-09-19 18:12:42 -0700

    added file

---
+++ 210-lab-12.cpp
@@ -0,0 +1,14 @@
+// COMSC210 | Lab 12 | Majd Bohsali
+// IDE used: Visual Studio Code
+
+#include <iostream>
+#include <algorithm>  // for sort(), find()
+#include <numeric>    // for accumulate()
+#include <array>
+#include <fstream>
+#include <iostream> 
+using namespace std;
+
+int main() { 
+    return 0;
+}
